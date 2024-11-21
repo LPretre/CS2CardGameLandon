a42ac61312980b4f09f779a4bbe679e452ad53a8
landonpretre <landon.pretre@menloschool.org>
2024-11-21 09:56:40 -0800

    Continued work on deck and started work on player

--- src/Deck.java
+++ src/Deck.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 public class Deck {
-    // Uses an ArrayList to store a deck of cards
+    /// Uses an ArrayList to store a deck of cards
     private ArrayList<Card> cards;
-    // Uses an integer to indicate how many cards are left in the deck
+    /// Uses an integer to indicate how many cards are left in the deck
     private int cardsLeft;
 
-    // Construct a deck of cards given suits and ranks
+    /// Construct a deck of cards given suits and ranks
     public Deck(String[] ranks, String[] suits, int[] values) {
         cards = new ArrayList<Card>();
         for (String suit : suits){
@@ -16,5 +16,37 @@ public class Deck {
         this.cardsLeft = suits.length * ranks.length;
     }
 
+    /// Indicate whether there are any cards that have not been dealt out
+    public boolean isEmpty(){
+        if (cardsLeft == 0){
+            return true;
+        }
+        return false;
+    }
 
+    public int getCardsLeft() {
+        return cardsLeft;
+    }
+
+    /// Deals out last card in the deck, given that the deck is not empty
+    public Card deal(){
+        if (isEmpty()) {
+            return null;
+        }
+        cardsLeft -= 1;
+        return cards.get(cardsLeft);
+
+    }
+
+    /// Swaps randomly indexed cards starting from the last card and going down to the first
+    /// Resets the cards left to the size of the deck
+    public void shuffle(){
+        for (int i = cards.size() - 1; i >= 0; i -= 1){
+            int newIndex = (int) (Math.random() * cards.size());
+            Card temp = cards.get(i);
+            cards.set(i, cards.get(newIndex));
+            cards.set(newIndex, temp);
+        }
+        cardsLeft = cards.size();
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+
 public class Player {
+    /// Stores player's name
+    private String name;
+    /// Stores the player's current hand
+    private ArrayList<Card> hand;
+    /// Stores the players points as an integer
+    private int points;
+
+    public Player(String name, ArrayList<Card> hand) {
+        this.name = name;
+        this.hand = hand;
+        this.points = 0;
+    }
+
+    public Player(String name) {
+        this.name = name;
+        this.points = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    /// Adds a given amount of points to the player
+    public void addPoints(int points){
+        this.points += points;
+    }
+
 }
